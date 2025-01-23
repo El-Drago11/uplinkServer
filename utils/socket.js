17df9c5306522eb2ba7b278de017c298da7c9714
unknown <11Rahulsinghrawat@gmail.com>
2025-01-24 02:31:07 +0530

    fixed socket functionality

--- utils/socket.js
+++ utils/socket.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const socket = require("socket.io");
 const { verifySocket } = require('../middleware/auth');
 const game = require('../models/game');
-
+const User = require('../models/User');
+const jwt = require("jsonwebtoken");
 
 const frontendUrl = process.env.FRONT_END_URL
 
@@ -35,14 +36,24 @@ const initializeTheSocket = (server) => {
 
     //step-2: when the web trying to connect with the backend
     io.on("connection", async (socket) => {
-        const gameId = socket.handshake.auth.gameId;
-        socket.on("click-event", async () => {
+        const token = socket.handshake.auth.token;
+        const decode = await jwt.verify(token, process.env.JWT_SECRET);
+        socket.join(decode.id.trim())
+        socket.on("click-event", async ({gameId}) => {
             try {
-                await game.findOneAndUpdate(
-                    { _id: gameId }, // Filter by gameId
-                    { $inc: { clickCount: 1 } }, // Increment click count
+                //step1: update user click in DB
+                const gameResp  = await game.findOneAndUpdate(
+                    { _id: gameId },
+                    { $inc: { clickCount: 1 } },
                 );
-                return;
+
+                //setp2: Get user details
+                const adminId = await User.findOne({accountType:'Admin'})
+                const adminRoomId = adminId._id.toString();
+                io.to(adminRoomId).emit('player-updated')
+                io.to(decode.id.trim()).emit('player-updated',{gameResp})
+                return ;
+
             } catch (err) {
                 console.error("Database operation failed:", err.message);
             }
